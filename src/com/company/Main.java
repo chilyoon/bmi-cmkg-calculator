073e98eaca653a736a912e2133a572019e506dfa
Devyash <devyashsanghai@gmail.com>
2017-02-09 16:47:39 -0500

    Initial Commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,27 @@
+package com.company;
+import java.awt.*;
+import javax.swing.*;
+import java.awt.event.*;
+import java.text.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+    }
+}
+
+class B1 extends JFrame{
+    Container c;
+    JLabel lbKG,lbFt,lblln,lblHT;
+    JTextField txtKg;
+    JComboBox cbFt,cbln;
+    JPanel p1,p2,p3;
+    JButton btnSubmit;
+
+    B1(){
+        c=getContentPane();
+        c.setLayout(new BoxLayout(c,BoxLayout.Y_AXIS));
+        p1.new JPanel(new )
+    }
+}
